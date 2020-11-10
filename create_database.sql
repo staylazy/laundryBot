ab241e153470f68fdc977cdfdfe2bfd97b006ab7
staylazy <47147510+staylazy@users.noreply.github.com>
2020-11-10 21:44:25 +0700

    Появился sql, создается таблица, некоторые мысли в комментах

---
+++ create_database.sql
@@ -0,0 +1,6 @@
+create table laundry_queue(
+    id integer primary key,
+    user varchar(255),
+    queue_date datetime
+    machine_num integer
+);
--- laundrybot.py
+++ laundrybot.py
@@ -1,12 +1,35 @@
 import telebot
+import db 
+bot = telebot.TeleBot('1480199090:AAGl4OdDzOH3cpyDGunLeQbVkCFHzK6Fw9s')
 
-bot = telebot.TeleBot('YOUR_TOKEN')
+# все это счастье в класс чтобы было прикольно
+# команды q aq dq 
+# по команде dq сдвинуть базу и удалить человека из жизни 
+# хачу кнопки
+# q выдает всю очередь
+# aq добавить себя после aq спрашивать да нет номер стиралки и тип стирки
+# узнать время стирки для каждого типа 
+a = [{'id': 1, 'user': 'Pidor baba', 'queue_date': 1, 'machine_num':1}, 
+     {'id': 1, 'user': 'Pidor baba', 'queue_date': 1, 'machine_num':1} ]
+def q():
+    pass
 
 @bot.message_handler(commands=['start','help'])
 def start(message):
     if message.text == '/start':
-        bot.reply_to(message,'Привет!')
+        bot.reply_to(message,'Привет!') # напиши хелп чтобы узнать что я могу
     if message.text == '/help':
-        bot.reply_to(message,'То - то ')
+        bot.reply_to(message,'То - то ') # расписать команды бота 
+
+@bot.message_handler(commands=['q', 'aq'])
+def queue(message):
+    if message.text == '/q':
+        bot.reply_to(message,'Сейчас расскажу всё, что знаю!')
+        q()
+
+
+
+    
+
 
 bot.polling()
